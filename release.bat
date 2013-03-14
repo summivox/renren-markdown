b899223b84b436a61d1d23b325d38d0311865b97
smilekzs <smilekzs@gmail.com>
2013-03-14 20:42:52 +0800

    migrate to grunt

--- release.bat
+++
@@ -1,1 +0,0 @@
-uglifyjs "renren-markdown.user.js" --comment --compress -o "release/renren-markdown.user.js"
---
+++ src/banner.js
@@ -0,0 +1,13 @@
+// Copyright (c) <%= grunt.template.today('yyyy') %>, <%= pkg.author.name %>. (MIT Licensed)
+// ==UserScript==
+// @name          <%= pkg.name %>
+// @namespace     http://github.com/smilekzs
+// @version       <%= pkg.version %>
+// @description   <%= pkg.description %>
+// @grant         GM_xmlhttpRequest
+// @grant         unsafeWindow
+// @include       *blog.renren.com/blog/*Blog*
+// @include       *blog.renren.com/blog/*edit*
+// @include       *blog.renren.com/*Entry*
+// ==/UserScript==
+// vim: set nowrap ft= : 
