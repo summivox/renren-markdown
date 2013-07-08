07cddcffba21646e2c696deda4903ba98ea2bca2
smilekzs <smilekzs@gmail.com>
2013-07-08 22:36:30 +0800

    fix for firefox + greasemonkey environment

--- src/gm/metadata.js
+++ src/gm/metadata.js
@@ -5,8 +5,11 @@
 // @version       <%= pkg.version %>
 // @description   <%= pkg.description %>
 // @grant         GM_xmlhttpRequest
-// @include       *blog.renren.com/blog/*Blog*
-// @include       *blog.renren.com/blog/*edit*
-// @include       *blog.renren.com/*Entry*
+// @match         *://blog.renren.com/blog/*Blog*
+// @match         *://blog.renren.com/blog/*edit*
+// @match         *://blog.renren.com/*Entry*
 // ==/UserScript==
 // vim: set nowrap ft= : 
+
+// http://stackoverflow.com/questions/4190442/run-greasemonkey-script-only-once-per-page-load
+if (window.top != window.self) return;  //don't run on frames or iframes
